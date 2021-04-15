abb8a3b942c1205148c9fb622af7a965b73d0e11
Bogdan Bryzh <bogdan.bryzh@gmail.com>
2021-04-15 08:55:21 +0300

    Refactored few files to ES modules

--- log.js
+++ log.js
@@ -1,8 +1,9 @@
-function log( ...args ) {
-    if ( args.length ) {
-        console.log( Date() + " - ", ...args );
-    } else {
-        console.log();
-    }
-}
-exports.log = log;
+const log = (...args) => {
+  if (args.length) {
+    console.log(Date() + ' - ', ...args);
+  } else {
+    console.log();
+  }
+};
+
+export default log;
